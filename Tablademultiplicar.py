eaeb8b6eb59fcc2c07b085d281b62e8865e0848e
luzdediana <changeme@example.com>
2024-06-28 22:30:30 +0000

    ya termine

---
+++ Tablademultiplicar.py
@@ -0,0 +1,16 @@
+###for x in range(1, 16):
+##multiplicacion = 1 * f
+##print("Ingresa cualquier numero:",f'1 x {f} = {multiplicacion}')
+
+
+f = int(input("Ingresa cualquier número: "))
+
+##enumeracion:
+for x in range(1, 16):
+    multiplicacion = x * f
+    print(f'{x} x {f} = {multiplicacion}')
+
+
+    
+
+
